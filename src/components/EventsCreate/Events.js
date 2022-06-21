65d5ad3e21e6fd90a5ce6c6faf01029d74ee14ef
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-06-22 05:23:31 +0800

    border style updted

--- src/components/EventsCreate/Events.js
+++ src/components/EventsCreate/Events.js
@@ -5,13 +5,14 @@ const Events = () => {
   const [items, setItems] = useState([]);
 
   const [className, setClassName] = useState(false);
-  console.log("get", items);
+
+  // console.log("get", items);
   function addToDo(e) {
     e.preventDefault();
     const value = e.target.task.value;
-    const id = value + Math.random().toString(36).slice(2);
+    const id = Math.random().toString(36).slice(2);
 
-    console.log(id, "p id");
+    // console.log(id, "p id");
 
     setItems((prevState) => {
       return [...prevState, { value: value, id: id }];
@@ -69,31 +70,41 @@ for (const item of items) {
 
   function dragStart(e) {
     // e.preventDefault();
-    setClassName(true);
-    const ki = e.dataTransfer.setData("Text", e.target.id);
-    console.log("drag started", ki);
-    /*  dragItem = this;
-  setTimeout(() => (this.className = "invisible"), 0) */
+    // setClassName(true);
+    e.dataTransfer.setData("Text", e.target.id);
+
+    const children = document.getElementsByClassName("child")
+    for (const child of children) {
+      console.log( e.target)
+      if (child.className === "child") {
+        child.style.border = "";
+      }
+    }
+
   }
 
   function dragOver(e) {
     e.preventDefault();
-    console.log("drag over");
+    // console.log("drag over");
+   
+ 
   }
   function dragEnter(e) {
-    console.log("drag entered", e);
-    if (e.target.className == "item") {
-      document.getElementById("demo").innerHTML = "Entered the dropzone";
+    if (e.target.className === "child") {
       e.target.style.border = "3px dotted red";
     }
   }
   function getDragLeft(e) {
-    if (e.target.className == "item") {
+    // console.log("drag left", e.target.className);
+    if (
+      e.target.className === "child" ||
+      e.target.className === "col-md-3" ||
+      e.target.className === "item"
+    ) {
       e.target.style.border = "";
     }
     // e.preventDefault();
     const length = e.target?.children?.length - 1;
-    console.log("drag left", length);
     const location = e.target?.children;
     // console.log("location", location[0]?.innerText);
     /*  for (const column of columns) {
@@ -107,90 +118,88 @@ for (const item of items) {
   }
 
   function dragDrop(e) {
-    e.preventDefault();
-    var data = e.dataTransfer.getData("Text");
-    console.log("whatr is ", data);
-    e.target.appendChild(document.getElementById(data));
-    // this.append(dragItem);
-    // setClassName(false);
+    // e?.target?.appendChild(document.getElementById(data));
+
     // console.log("drag dropped",dragItem.getElementsByTagName("span"));
     const length = e.target?.children?.length;
     const location = e.target?.children;
-    // console.log("location Drop", location[0]?.innerText);
-    console.log("Drag Drop", length);
+    // console.log("location Drop", location[0]);
+    // console.log("Drag Drop", length);
     // console.log("children",e.target?.children)
+    if (!location[0]) {
+      return false;
+    } else {
+      e.preventDefault();
+      const data = e.dataTransfer.getData("Text");
+      e?.target?.appendChild(document.getElementById(data));
+      console.log(e.target.className);
+    }
 
-    /*  for (const column of columns) {
-      if (location[0]?.innerText == "All Tasks") {
-        continue;
-      } */
-    /*  if (
-          column.firstChild.nextSibling.innerText == location[0].innerText
-        ) {
-        } */
-    // console.log(column.getElementsByTagName("span")?.namedItem?.length);
+    // const h1 = document.getElementsByClassName("unselectable");
+    // for (const iterator of h1)  {
+    //   console.log("what is drop", iterator.getElementsByTagName("span").length);
+    //   if (iterator.getElementsByTagName("span").length === 1) {
+    //     console.log("https://", iterator)
+    //     // return false
+    //   }
+    //   else {
 
-    // console.log(column.getElementsByTagName("span").length)
-    /* if(column.childElementCount > 2){
-          console.log("you have add two or more")
+    //   }
 
-        } */
-    /*   if (length > 3) {
-        this.className = "warning column";
-        console.warn("length is greeter then two");
-      }
-    } */
+    // }
   }
 
   function dragEnd(e) {
-    console.log("drag ended", e);
+    // console.log("drag ended", e);
     setClassName(false);
-    /* this.className = "item";
-  dragItem = null; */
   }
   const monthsDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
-      <section
-        className="column"
-        id="addNew"
-        onDragOver={dragOver}
-        onDragEnter={dragEnter}
-        onDragLeave={getDragLeft}
-        onDrop={dragDrop}
-      >
-        <h1>Create an Event</h1>
-      <form id="form" onSubmit={addToDo}>
-        <input className="formInline" type="text" name="task" required />
-        <input className="formInline" type="submit" value="Add" />
-      </form>
-        {items.map((item, key) => (
-          <span
-            className="item"
-            draggable="true"
-            key={item.id || key}
-            id={item.id}
-            onDragStart={dragStart}
-            onDragEnd={dragEnd}
-          ><small>&#10004;</small>{item?.value}
-          </span>
-        ))}
-      </section>
-      <section className="parent">
-
-      {monthsDays.map((day) => (
-        <div
-          key={day}
-          className="child"
+      <h3 className="text-center my-5">June 22, 2022</h3>
+      <div className="row">
+        <section
+          className="col-md-3"
+          id="addNew"
           onDragOver={dragOver}
           onDragEnter={dragEnter}
           onDragLeave={getDragLeft}
           onDrop={dragDrop}
         >
-          <h1>{day}</h1>
-        </div>
-      ))}
-      </section>
+          <h4>Create an Event</h4>
+          <form id="form" onSubmit={addToDo}>
+            <input className="formInline" type="text" name="task" required />
+            <input className="formInline" type="submit" value="Add" />
+          </form>
+          {items.map((item, key) => (
+            <span
+              className="item"
+              draggable="true"
+              key={item.id || key}
+              id={item.id}
+              onDragStart={dragStart}
+              onDragEnd={dragEnd}
+            >
+              <small>&#10004;</small>
+              {item?.value}
+            </span>
+          ))}
+        </section>
+        <section className="parent col-md-8">
+          {monthsDays.map((day) => (
+            <div
+              key={day}
+              className="child"
+              onDragOver={dragOver}
+              onDragEnter={dragEnter}
+              onDragLeave={getDragLeft}
+              onDrop={dragDrop}
+            >
+              <h1 id="unselectable">{day}</h1>
+            </div>
+          ))}
+        </section>
+      </div>
     </>
   );
 };
